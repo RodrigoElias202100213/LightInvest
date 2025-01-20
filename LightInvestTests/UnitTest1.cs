ed6f36ba7bde7e266b08f2224086cd4507e49cfe
veragfernandes <127191676+veragfernandes@users.noreply.github.com>
2025-01-20 23:35:48 +0000

    projeto testes inicio

---
+++ LightInvestTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace LightInvestTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- LightInvestTests/obj/Debug/net8.0/LightInvestTests.AssemblyInfo.cs
+++ LightInvestTests/obj/Debug/net8.0/LightInvestTests.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LightInvest")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("LightInvestTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+936363ae0c317b3b7034f603a3bd9ca20aa8f300")]
-[assembly: System.Reflection.AssemblyProductAttribute("LightInvest")]
-[assembly: System.Reflection.AssemblyTitleAttribute("LightInvest")]
+[assembly: System.Reflection.AssemblyProductAttribute("LightInvestTests")]
+[assembly: System.Reflection.AssemblyTitleAttribute("LightInvestTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
