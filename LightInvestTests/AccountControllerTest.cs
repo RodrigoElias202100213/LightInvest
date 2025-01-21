d08335012a8d585c7b0b4b2b4351a989fd315c65
veragfernandes <127191676+veragfernandes@users.noreply.github.com>
2025-01-21 01:53:59 +0000

    2 testes feitos

--- LightInvestTests/AccountControllerTest.cs
+++ LightInvestTests/AccountControllerTest.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LightInvest.Data;
+using LightInvest.Models;
+using LightInvest.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Moq;
 
 namespace LightInvestTests
 {
-    internal class AccountControllerTest
+    public class AccountControllerTest
     {
-        //testes do account controller
+
+        [Fact]
+        public void Login_ReturnsView()
+        {
+            var ac = new AccountController(null);
+            var result = ac.Login();
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Register_ReturnsViewWithRegisterViewModel()
+        {
+            var ac = new AccountController(null);
+            var result = ac.Register();
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.IsType<RegisterViewModel>(viewResult.Model);
+        }
     }
 }
