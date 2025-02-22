df8db7f3f574e65dc696e820e301a95093fe6dff
Ivandro Antunes <95924390+IIvandro@users.noreply.github.com>
2025-02-22 15:29:47 +0000

    calculo ROI

---
+++ LightInvest/Controllers/ROICalculatorController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using LightInvest.Models; // Certifique-se de que este namespace está correto
+
+namespace LightInvest.Controllers
+{
+    public class ROICalculatorController : Controller
+    {
+        // GET: ROICalculator
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Calcular(ROICalculator model)
+        {
+            if (ModelState.IsValid)
+            {
+                decimal resultadoROI = model.CalcularROI();
+                ViewBag.Resultado = resultadoROI;
+            }
+            else
+            {
+                ViewBag.Resultado = "Valores inválidos!";
+            }
+            return View("Index", model);
+        }
+    }
+}
