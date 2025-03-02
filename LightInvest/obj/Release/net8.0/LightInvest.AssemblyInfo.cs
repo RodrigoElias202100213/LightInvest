aac5e59cbc7d9d14152826acb1b535bd513bd3c6
rodri <202100213@estudantes.ips.pt>
2025-03-02 14:27:27 +0000

    ..

--- LightInvest/obj/Release/net8.0/LightInvest.AssemblyInfo.cs
+++ LightInvest/obj/Release/net8.0/LightInvest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LightInvest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65c78acd0248896bdd1d020a87b9f601c5df65a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c08801f7375ba41ed8d881b34a873a68f4b4dce")]
 [assembly: System.Reflection.AssemblyProductAttribute("LightInvest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LightInvest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
