ee5e28a3dbc6861254a030136b67f81483eebce9
veragfernandes <127191676+veragfernandes@users.noreply.github.com>
2025-03-02 14:26:17 +0000

    Revert "tentativa"
    This reverts commit d27430dcc91f647ec3b644a72ee5e1bd265b84a7.

---
+++ LightInvest/Migrations/20250228191911_InitialCommunityMigration.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LightInvest.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCommunityMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ROICalculators",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustoInstalacao = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CustoManutencaoAnual = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ConsumoEnergeticoMedio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ConsumoEnergeticoRede = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    RetornoEconomia = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ROI = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DataCalculado = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ROICalculators", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ROICalculators");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- LightInvest/Program.cs
+++ LightInvest/Program.cs
@@ -6,10 +6,9 @@ using LightInvest.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
-        sqlServerOptions => sqlServerOptions.EnableRetryOnFailure()));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
+	
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = "/Account/Login";
