e0108ecf79884143c861d6fa12a5852f14952dfc
Ivandro Antunes <95924390+IIvandro@users.noreply.github.com>
2025-03-04 09:56:09 +0000

    teste de integração

---
+++ LightInvestTest/ExternalApiServiceMock.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+public class ExternalApiServiceMock
+{
+    [Fact]
+    public async Task Simular_ApiDeTerceiros()
+    {
+        var mock = new Mock<IExternalApiService>();
+
+        // Simula um retorno esperado da API
+        mock.Setup(api => api.ObterTaxaCambioAsync("USD"))
+            .ReturnsAsync(5.25m);
+
+        var taxa = await mock.Object.ObterTaxaCambioAsync("USD");
+
+        Assert.Equal(5.25m, taxa);
+    }
+}
+
+public interface IExternalApiService
+{
+    Task<decimal> ObterTaxaCambioAsync(string moeda);
+}
---
+++ LightInvestTest/ROICalculatorIntegrationTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using LightInvest;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+public class ROICalculatorIntegrationTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public ROICalculatorIntegrationTests(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Calcular_RetornaROI_ComSucesso()
+    {
+        var requestBody = new
+        {
+            CustoInstalacao = 50000,
+            CustoManutencaoAnual = 1000,
+            ConsumoEnergeticoMedio = 200,
+            ConsumoEnergeticoRede = 500,
+            RetornoEconomia = 1000
+        };
+
+        var jsonContent = new StringContent(JsonSerializer.Serialize(requestBody), Encoding.UTF8, "application/json");
+
+        var response = await _client.PostAsync("/ROICalculator/Calcular", jsonContent);
+
+        response.EnsureSuccessStatusCode();
+
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains("ROI Calculado", responseString);
+    }
+
+    [Fact]
+    public async Task Calcular_DeveRetornarErro_QuandoEconomiaZero()
+    {
+        var requestBody = new
+        {
+            CustoInstalacao = 50000,
+            CustoManutencaoAnual = 1000,
+            ConsumoEnergeticoMedio = 200,
+            ConsumoEnergeticoRede = 500,
+            RetornoEconomia = 0 // Economia inválida
+        };
+
+        var jsonContent = new StringContent(JsonSerializer.Serialize(requestBody), Encoding.UTF8, "application/json");
+
+        var response = await _client.PostAsync("/ROICalculator/Calcular", jsonContent);
+
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains("Erro: A economia total deve ser maior que zero!", responseString);
+    }
+}
+
