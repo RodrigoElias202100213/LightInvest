c5bdfd554963347f8c28843faed4bae6057356d9
rodri <202100213@estudantes.ips.pt>
2025-03-06 20:30:54 +0000

    test

--- LightInvestTest/obj/Release/net8.0/LightInvestTest.AssemblyInfo.cs
+++ LightInvestTest/obj/Release/net8.0/LightInvestTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LightInvestTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+018449f600e02fe523b473c6c21dafb332a7d02f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a45c9b24743da626316232e55f0522b329b522f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LightInvestTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LightInvestTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
