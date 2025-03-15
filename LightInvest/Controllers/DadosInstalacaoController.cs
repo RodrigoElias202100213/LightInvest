0a07606374d5c0de0f8da269249573bdb184440b
rodri <202100213@estudantes.ips.pt>
2025-03-15 03:46:48 +0000

    tentativa

--- LightInvest/Controllers/DadosInstalacaoController.cs
+++ LightInvest/Controllers/DadosInstalacaoController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using System.Linq;
 using LightInvest.Models.b;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace LightInvest.Controllers
@@ -30,13 +31,82 @@ namespace LightInvest.Controllers
 		[HttpGet]
 		public async Task<IActionResult> Create()
 		{
-			var cidades = await _context.Cidades.ToListAsync() ?? new List<Cidade>();
-			var modelos = await _context.ModelosDePaineisSolares.ToListAsync() ?? new List<ModeloPainelSolar>();
+			var user = await GetLoggedInUserAsync();
+			if (user == null)
+			{
+				ViewBag.Resultado = "Erro: Nenhum utilizador autenticado!";
+				return View();
+			}
 
-			ViewBag.Cidades = new SelectList(cidades, "Id", "Nome");
-			ViewBag.ModelosPainel = new SelectList(modelos, "Id", "Nome");
+			var dadosInstalacao = await _context.DadosInstalacao
+				.FirstOrDefaultAsync(d => d.UserEmail == user.Email);
 
-			return View(new DadosInstalacao());
+			if (dadosInstalacao == null)
+			{
+				dadosInstalacao = new DadosInstalacao
+				{
+					UserEmail = user.Email,
+					CidadeId = 0,
+					ModeloPainelId = 0,
+					NumeroPaineis = 0,
+					ConsumoPainel = 0,
+					Inclinacao = 0,
+					Dificuldade = "fácil"
+				};
+
+				_context.DadosInstalacao.Add(dadosInstalacao);
+				await _context.SaveChangesAsync();
+			}
+
+			ViewBag.Cidades = new SelectList(_context.Cidades, "Id", "Nome");
+			ViewBag.Modelos = new SelectList(_context.ModelosDePaineisSolares, "Id", "Nome");
+
+			return View(dadosInstalacao);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create(DadosInstalacao model)
+		{
+			var user = await GetLoggedInUserAsync();
+			if (user == null)
+			{
+				ViewBag.Resultado = "Erro: Nenhum utilizador autenticado!";
+				return View(model);
+			}
+
+			var dadosInstalacao = await _context.DadosInstalacao
+				.FirstOrDefaultAsync(d => d.UserEmail == user.Email);
+
+			if (dadosInstalacao == null)
+			{
+				dadosInstalacao = new DadosInstalacao
+				{
+					UserEmail = user.Email,
+					CidadeId = model.CidadeId,
+					ModeloPainelId = model.ModeloPainelId,
+					NumeroPaineis = model.NumeroPaineis,
+					ConsumoPainel = model.ConsumoPainel,
+					Inclinacao = model.Inclinacao,
+					Dificuldade = model.Dificuldade
+				};
+
+				_context.DadosInstalacao.Add(dadosInstalacao);
+			}
+			else
+			{
+				dadosInstalacao.CidadeId = model.CidadeId;
+				dadosInstalacao.ModeloPainelId = model.ModeloPainelId;
+				dadosInstalacao.NumeroPaineis = model.NumeroPaineis;
+				dadosInstalacao.ConsumoPainel = model.ConsumoPainel;
+				dadosInstalacao.Inclinacao = model.Inclinacao;
+				dadosInstalacao.Dificuldade = model.Dificuldade;
+
+				_context.DadosInstalacao.Update(dadosInstalacao);
+			}
+
+			await _context.SaveChangesAsync();
+
+			return RedirectToAction("Index");
 		}
 
 		public async Task<IActionResult> GetPotenciasByModelo(int modeloId)
