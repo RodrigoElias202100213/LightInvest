f35c0db3c4b8e9449e569f27c0b6238fac288516
rodri <202100213@estudantes.ips.pt>
2025-03-15 02:37:32 +0000

    versao 4.2.1 simualcao nao a funcionar

---
+++ LightInvest/Models/b/ModeloPainelSolar.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LightInvest.Models.b
+{
+	public class ModeloPainelSolar
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(100)]
+		public string Modelo { get; set; }
+
+		[InverseProperty("PainelSolar")]
+		public List<PotenciaPainelSolar> Potencias { get; set; } // Lista de potências
+	}
+}
