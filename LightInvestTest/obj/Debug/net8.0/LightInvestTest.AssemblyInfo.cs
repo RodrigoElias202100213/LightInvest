3698b8bb7a2a4c0eadc71879cbfe565a86a7ca3b
rodri <202100213@estudantes.ips.pt>
2025-03-16 18:39:44 +0000

    simlucao graficos.

--- LightInvestTest/obj/Debug/net8.0/LightInvestTest.AssemblyInfo.cs
+++ LightInvestTest/obj/Debug/net8.0/LightInvestTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LightInvestTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c272cfb8cbaca3ce146717b39740df1a384a3263")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c2801e8876136c72048e3a58f1abbfe7a0b76b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LightInvestTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LightInvestTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- LightInvestTest/obj/Release/net8.0/LightInvestTest.AssemblyInfo.cs
+++ LightInvestTest/obj/Release/net8.0/LightInvestTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LightInvestTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+972cb4e83ee370ffa5a931926d830dd9497a0d98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c2801e8876136c72048e3a58f1abbfe7a0b76b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LightInvestTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LightInvestTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
