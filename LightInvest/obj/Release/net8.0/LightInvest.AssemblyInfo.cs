9e8be56d6bee3d2fe0ab59f037c289c9dff753e6
rodri <202100213@estudantes.ips.pt>
2025-03-24 19:29:37 +0000

    correcao da usabilidade

--- LightInvest/obj/Release/net8.0/LightInvest.AssemblyInfo.cs
+++ LightInvest/obj/Release/net8.0/LightInvest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LightInvest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("8.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("8.0.0+f31d0a6044fa08a4acc12b30c2d58791d3c1683d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("8.0.0+aeeb817bbe7964c3cab9dea1bb4540e63812ac30")]
 [assembly: System.Reflection.AssemblyProductAttribute("LightInvest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LightInvest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("8.0.0.0")]
