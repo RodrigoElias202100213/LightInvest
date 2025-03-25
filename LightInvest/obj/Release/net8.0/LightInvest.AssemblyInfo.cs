9115239863d3f4544b27b053da3cf168783515a1
veragfernandes <127191676+veragfernandes@users.noreply.github.com>
2025-03-25 16:48:16 +0000

    Versão 8.2.1

--- LightInvest/obj/Release/net8.0/LightInvest.AssemblyInfo.cs
+++ LightInvest/obj/Release/net8.0/LightInvest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LightInvest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("8.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("8.0.0+c18a3d2168d2312a5875d21406d6efbfbd839e41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("8.0.0+35cbe9876eeb5d59f58149c83f837584aeebdac9")]
 [assembly: System.Reflection.AssemblyProductAttribute("LightInvest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LightInvest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("8.0.0.0")]
