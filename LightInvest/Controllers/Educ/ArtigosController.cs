c8298586c2f37cfcc34992a64dd7950de5b86752
veragfernandes <127191676+veragfernandes@users.noreply.github.com>
2025-03-29 18:10:42 +0000

    Versão 8.4
    Implementação da API dos artigos

--- LightInvest/Controllers/Educ/ArtigosController.cs
+++ LightInvest/Controllers/Educ/ArtigosController.cs
@@ -1,54 +1,59 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using LightInvest.Models;
 using LightInvest.Models.BD;
 using Markdig;
 using LightInvest.Models.Educ.Artigos;
-
+using LightInvest.Services;
 
 namespace LightInvest.Controllers.Educ
 {
-	public class ArtigosController : Controller
-	{
-		private readonly ApplicationDbContext _context;
+    public class ArtigosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly MediaStackService _mediaStackService;
 
-		public ArtigosController(ApplicationDbContext context)
-		{
-			_context = context;
-		}
+        public ArtigosController(ApplicationDbContext context, MediaStackService mediaStackService)
+        {
+            _context = context;
+            _mediaStackService = mediaStackService;
+        }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-		public IActionResult ListarPorCategoria(string categoria)
-		{
-			var artigos = _context.Artigos.Where(a => a.Categoria == categoria).ToList();
-			ViewBag.Categoria = categoria;
-			return View(artigos);
-		}
-		public IActionResult Detalhes(int id)
-		{
-			var artigo = _context.Artigos.FirstOrDefault(a => a.ArtigoId == id);
-			if (artigo == null)
-			{
-				return NotFound();
-			}
+        public IActionResult ListarPorCategoria(string categoria)
+        {
+            var artigos = _context.Artigos.Where(a => a.Categoria == categoria).ToList();
+            ViewBag.Categoria = categoria;
+            return View(artigos);
+        }
 
-			var htmlConteudo = Markdown.ToHtml(artigo.Conteudo);
+        public async Task<IActionResult> Detalhes(int id)
+        {
+            var artigo = _context.Artigos.FirstOrDefault(a => a.ArtigoId == id);
+            if (artigo == null)
+            {
+                return NotFound();
+            }
 
-			ViewBag.ConteudoHtml = htmlConteudo;
+            var htmlConteudo = Markdown.ToHtml(artigo.Conteudo);
+            ViewBag.ConteudoHtml = htmlConteudo;
 
-			var artigosRelacionados = _context.Artigos
-											   .Where(a => a.Categoria == artigo.Categoria && a.ArtigoId != artigo.ArtigoId)
-											   .Take(3)
-											   .ToList();
-			artigo.ArtigosRelacionados = artigosRelacionados ?? new List<Artigo>();
+            var artigosRelacionados = _context.Artigos
+                .Where(a => a.Categoria == artigo.Categoria && a.ArtigoId != artigo.ArtigoId)
+                .Take(3)
+                .ToList();
+            artigo.ArtigosRelacionados = artigosRelacionados ?? new List<Artigo>();
 
-			return View(artigo);
-		}
+            var noticiasRelacionadas = await _mediaStackService.GetSolarPanelArticlesAsync();
+            ViewBag.NoticiasRelacionadas = noticiasRelacionadas;
 
-	}
+            return View(artigo);
+        }
+    }
 }
---
+++ LightInvest/Models/NovaPasta/EmailService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LightInvest.Services
+{
+    public class MediaStackService
+    {
+        private readonly HttpClient _httpClient;
+        private const string ApiKey = "73261e3e3f837ec6c829b44371ae2ad7";
+        private const string BaseUrl = "http://api.mediastack.com/v1/news";
+
+        public MediaStackService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<NewsArticle>> GetSolarPanelArticlesAsync()
+        {
+            var url = $"{BaseUrl}?access_key={ApiKey}&categories=general&languages=en&keywords=solar panels";
+            var response = await _httpClient.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return new List<NewsArticle>();
+            }
+
+            var json = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<MediaStackResponse>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            return result?.Data ?? new List<NewsArticle>();
+        }
+    }
+
+    public class MediaStackResponse
+    {
+        public List<NewsArticle> Data { get; set; }
+    }
+
+    public class NewsArticle
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string Image { get; set; }
+    }
+}
