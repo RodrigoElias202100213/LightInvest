507dc84b4ba6f0ecdce26723362b33c1e028b40c
rodri <202100213@estudantes.ips.pt>
2025-03-30 21:53:56 +0100

    8.7.3 INTERFACE ARTIGOS E MANUAL

--- LightInvestTest/obj/Debug/net8.0/LightInvestTest.AssemblyInfo.cs
+++ LightInvestTest/obj/Debug/net8.0/LightInvestTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LightInvestTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65aaace62074d1ab8156725184610ec7ea375aef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a882c3621b12e1833c02e3b9b346db806a49f3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("LightInvestTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LightInvestTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
