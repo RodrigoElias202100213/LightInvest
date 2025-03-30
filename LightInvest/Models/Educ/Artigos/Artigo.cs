90191f7f57564172ad37daf452bfcac53fdcfe2d
veragfernandes <127191676+veragfernandes@users.noreply.github.com>
2025-03-30 13:57:24 +0100

    Versão 8.5.4

--- LightInvest/Models/Educ/Artigos/Artigo.cs
+++ LightInvest/Models/Educ/Artigos/Artigo.cs
@@ -1,31 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace LightInvest.Models.Educ.Artigos
 {
-	public class Artigo
-	{
-		[Key]
-		public int ArtigoId { get; set; }
+    /// <summary>
+    /// Represents an article with content, category, related articles, and comments.
+    /// </summary>
+    public class Artigo
+    {
+        /// <summary>
+        /// Unique identifier for the article.
+        /// </summary>
+        [Key]
+        public int ArtigoId { get; set; }
 
-		[Required]
-		[MaxLength(200)]
-		public string Titulo { get; set; }
+        /// <summary>
+        /// Title of the article.
+        /// </summary>
+        [Required]
+        [MaxLength(200)]
+        public string Titulo { get; set; }
 
-		[Required]
-		public string Conteudo { get; set; }
+        /// <summary>
+        /// Main content of the article.
+        /// </summary>
+        [Required]
+        public string Conteudo { get; set; }
 
-		public string ImagemUrl { get; set; }
+        /// <summary>
+        /// URL of the article's image.
+        /// </summary>
+        public string ImagemUrl { get; set; }
 
-		public string Categoria { get; set; }
-		[MaxLength(500)]
-		public string DescricaoCurta { get; set; }
+        /// <summary>
+        /// Category to which the article belongs.
+        /// </summary>
+        public string Categoria { get; set; }
 
-		public DateTime DataPublicacao { get; set; } = DateTime.UtcNow;
-		
-		public List<Artigo> ArtigosRelacionados { get; set; }
-		
-		public List<Comentario> Comentarios { get; set; } = new List<Comentario>();
+        /// <summary>
+        /// Short description of the article.
+        /// </summary>
+        [MaxLength(500)]
+        public string DescricaoCurta { get; set; }
 
-	}
+        /// <summary>
+        /// Date when the article was published.
+        /// </summary>
+        public DateTime DataPublicacao { get; set; } = DateTime.UtcNow;
+
+        /// <summary>
+        /// List of related articles.
+        /// </summary>
+        public List<Artigo> ArtigosRelacionados { get; set; }
+
+        /// <summary>
+        /// List of comments associated with the article.
+        /// </summary>
+        public List<Comentario> Comentarios { get; set; } = new List<Comentario>();
+    }
 }
--- LightInvest/obj/Release/net8.0/LightInvest.AssemblyInfo.cs
+++ LightInvest/obj/Release/net8.0/LightInvest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LightInvest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("8.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("8.0.0+211642e6e93020a342561b1c64f4413471702dce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("8.0.0+13b5699f2bdeaf69fc40d612d90aff613fda4719")]
 [assembly: System.Reflection.AssemblyProductAttribute("LightInvest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LightInvest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("8.0.0.0")]
