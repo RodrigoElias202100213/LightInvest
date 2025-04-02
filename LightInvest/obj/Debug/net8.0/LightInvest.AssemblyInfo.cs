b0d9b070974ca68b1415860744805b66e1d3ecad
veragfernandes <127191676+veragfernandes@users.noreply.github.com>
2025-04-02 11:00:34 +0100

    Versão 10.3

--- LightInvest/obj/Debug/net8.0/LightInvest.AssemblyInfo.cs
+++ LightInvest/obj/Debug/net8.0/LightInvest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LightInvest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("8.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("8.0.0+10793ff2c1704cd90dddf19d95afe7896faca25a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("8.0.0+cd9931bfa53ec2c3f6dce001316487924d0254ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("LightInvest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LightInvest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("8.0.0.0")]
