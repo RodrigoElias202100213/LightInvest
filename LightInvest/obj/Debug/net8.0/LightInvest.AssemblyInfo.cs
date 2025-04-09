395fab4ff7897da16e06b8e5b7c15a79f02b9755
rodri <202100213@estudantes.ips.pt>
2025-04-09 22:08:51 +0100

    .

--- LightInvest/obj/Debug/net8.0/LightInvest.AssemblyInfo.cs
+++ LightInvest/obj/Debug/net8.0/LightInvest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LightInvest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("8.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("8.0.0+b02d63bab4581814eb91a7d6522a1a0f6509006d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("8.0.0+5268986abc32eea9746c73f77c72ef86738b6e13")]
 [assembly: System.Reflection.AssemblyProductAttribute("LightInvest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LightInvest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("8.0.0.0")]
